8357352b9b8456e3b8cd95a185f9d18375a82e29
Ryan2414 <dltmdwo2418@gmail.com>
2020-12-01 18:59:21 +0900

    케릭터 만들기
    유튜브 1강 따라하기
    케릭터 화살표 방향 따라 움직이기, 마우스 방향 바라보기 구현

---
+++ Make Game/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (PlayerController))]
+public class Player : MonoBehaviour
+{
+
+    public float movespeed = 5;
+
+    Camera viewCamera;
+
+    PlayerController controller;
+
+    
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+
+    }
+
+   
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moneVelocity = moveInput.normalized * movespeed;
+        controller.Move(moneVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if(groundPlane.Raycast(ray,out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            //Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+
+        }
+    }
+}
